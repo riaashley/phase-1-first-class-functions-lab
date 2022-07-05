cbb1317048d7f206f4f357022e5b00e03c11d9a8
Ria Legaspi <riaveluz@gmail.com>
2022-07-05 11:01:00 -0700

    commit tree 7cc7ccecd41b6d2b5a62c229fa43d9785c05e295

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    console.log(['Antonia', 'Nuru', 'Amari', 'Mo'])
+    return ['Antonia', 'Nuru']
+};
+
+const returnLastTwoDrivers = function () {
+    console.log(['Antonia', 'Nuru', 'Amari', 'Mo'])
+    return ['Amari', 'Mo']
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+     return function(number) {
+         return number * x
+     }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(names, drivers){
+    return drivers (names) 
+}
+
+function driversNames(names){
+    names.slice(0,2)
+}
